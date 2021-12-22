103446be13798946f1386a7296ed506f9f557e9d
auba86 <audrius.bareisis@gmail.com>
2021-12-22 10:08:52 +0200

    New

--- javascript.js
+++ javascript.js
@@ -1,9 +1,31 @@
-const btn = document.querySelector('button');
+const turinys = document.querySelector('input');
 
+const container = document.querySelector('.container');
 
+turinys.addEventListener('input', (e) => {
+  e.preventDefault();
 
-function changePosition() {
-    btn.style.position.left = "10px";
-    btn.style.position.bottom = "10px";
+  let inputName = String(document.querySelector('input[name=vardas]').value);
 
-}
+  if(inputName === "Giedrė" || inputName === "Giedre") {
+            container.style.backgroundColor = "red";
+
+            document.querySelector('#result').textContent = `${inputName} yra visiška lenkė, bl.`
+        }
+        else{
+            container.style.backgroundColor = "green";
+
+            const last = inputName.charAt(inputName.length - 1);
+
+            if(last === 's') {
+
+            document.querySelector('#result').textContent = `${inputName} yra lietuvis`
+            }
+            else if(last === 'ė' || last === 'a'){
+                document.querySelector('#result').textContent = `${inputName} yra lietuvė`
+            }
+        }
+
+        
+});
+
